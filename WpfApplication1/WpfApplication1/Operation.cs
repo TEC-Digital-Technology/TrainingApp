111995a0cf69c57eb5f212615b181f4a6c6f90ad
曾彥 <Antony77@tecyt.com>
2016-12-10 19:48:07 +0800

    1. 作業=> 把 生產者/消費者 的網頁版移植到 WPF

---
+++ WpfApplication1/WpfApplication1/Operation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEC.Core.ComponentModel;
+
+namespace WpfApplication1
+{
+    [DescriptiveEnumEnforcement(EnforcementType = DescriptiveEnumEnforcementAttribute.EnforcementTypeEnum.ThrowException)]
+    public enum Operation
+    {
+        [EnumDescription("+")]
+        Add,
+        [EnumDescription("-")]
+        Substract,
+        [EnumDescription("*")]
+        Multiple,
+        [EnumDescription("/")]
+        Divide
+    }
+}
